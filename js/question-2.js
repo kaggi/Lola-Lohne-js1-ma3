612a5305749d884bc63d8ca9acb4c0fc461a786d
kaggi <lola.lohne@gmail.com>
2022-04-23 12:15:29 +0200

    Question 2

--- js/question-2.js
+++ js/question-2.js
@@ -3,7 +3,27 @@
 
 console.log("Question 2");
 
+const displayGamesDiv = document.querySelector(".displayGames");
+
+displayGamesDiv.innerHTML="";
+
+function showResults(games) {
+    for (let i = 0; i < games.length; i++) {
+        let gameName = games[i].name;
+        let gameRating = games[i].rating;
+        let gameTags = games[i].tags.length;
+
+        displayGamesDiv.innerHTML += `<div class="gameInfo"><p></p>Name: ${gameName}<p></p>Rating: ${gameRating}<p>Number of tags: ${gameTags}</p></div>`
+        if(i===7){
+            break
+        }        
+
+    }
+}
+
 const url = 'https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=ef04ada82d234eee94ed92702438552e';
+
+
 
 async function getGames() {
     try {
@@ -18,19 +38,9 @@ async function getGames() {
         
 
     } catch (error) {
-        console.log(error);
-        //resultsContainer.innerHTML = alert("error", error);
+       console.log(error);
+       displayGamesDiv.innerHTML = "An error occured";
     }
 }
 
 getGames();
-
-function showResults(games) {
-    for (let i = 0; i < 8; i++) {
-        console.log("Game " + (i+1));
-        console.log(games[i].name);
-        console.log(games[i].rating);
-        console.log(games[i].tags.length);          
-
-    }
-}
